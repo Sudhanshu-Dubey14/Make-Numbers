72b7031f62ff28bad392943c46252d8648134423
Sudhanshu-Dubey14 <sudhanshu.thethunder@gmail.com>
2019-07-23 00:34:20 +0530

    Newline character added at the end of lines

--- src/functions.cpp
+++ src/functions.cpp
@@ -8,31 +8,31 @@ int main()
   cout<<" Enter the number : ";
   cin>>num;
   cout<<" What do you want to check : \n 1.Is number prime?"; 
-  cout<<"\n 2.Is it armstrong number? \n 3. Is number a palindrome?";
+  cout<<"\n 2.Is it armstrong number? \n 3. Is number a palindrome?\n";
   cin>>opt;
   switch(opt)
   { case 1 : ans = isprime(num);
 	     if( ans = 'y')
-		     cout<<num<<" is a prime number.";
+		     cout<<num<<" is a prime number.\n";
 	     else if( ans = 'n' )
-		     cout<<num<<" is not a prime number.";
+		     cout<<num<<" is not a prime number.\n";
 	     break;
 
     case 2 : ans = isarmstrong(num);
 	     if( ans = 'y')
-		     cout<<num<<" is a armstrong number.";
+		     cout<<num<<" is a armstrong number.\n";
 	     else if( ans = 'n' )
-		     cout<<num<<" is not a armstrong number.";
+		     cout<<num<<" is not a armstrong number.\n";
      	     break;
 
     case 3 : ans = ispalindrome(num);
 	     if( ans = 'y')
-		     cout<<num<<" is a palindrome.";
+		     cout<<num<<" is a palindrome.\n";
 	     else if( ans = 'n' )
-		     cout<<num<<" is not a palindrome.";
+		     cout<<num<<" is not a palindrome.\n";
 	     break;
 
-    default : cout<<" Invalid input !!!"; 
+    default : cout<<" Invalid input !!!\n"; 
   }
   return 0;
 }  
